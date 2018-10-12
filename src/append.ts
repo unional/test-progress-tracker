c3ef8621385e635b46669337039163ad6cfbb83f
Homa Wong <homawong@gmail.com>
2018-10-12 13:59:39 -0700

    make context optional

--- src/append.ts
+++ src/append.ts
@@ -1,13 +1,16 @@
-import path from 'path'
+import fs from 'fs';
+import path from 'path';
 import { compress } from './compress';
+import { ROOT, TEST_RESULT_FILENAME } from './constants';
 import { Context, TestResults } from './interface';
 import { minify } from './minify';
-import { TEST_RESULT_FILENAME } from './constants';
+import { unpartial } from 'unpartial';
 
-export function append({ fs, rootDir }: Context, results: TestResults) {
+export function append(context: Partial<Context> | undefined, results: TestResults) {
+  const c = unpartial({ fs, rootDir: ROOT }, context)
   const minified = minify(results)
   const compressed = compress(minified)
 
-  const filepath = path.join(rootDir, TEST_RESULT_FILENAME)
-  fs.appendFileSync(filepath, compressed + '\n')
+  const filepath = path.join(c.rootDir, TEST_RESULT_FILENAME)
+  c.fs.appendFileSync(filepath, compressed + '\n')
 }
