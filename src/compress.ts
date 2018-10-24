987f06c1e3ce822ffb105723bc8c5e7cdb4f1668
Homa Wong <homawong@gmail.com>
2018-10-23 18:15:18 -0700

    fix: disable compress
    The compression is causing some problems.
    Sometimes the compressed string can't be uncompressed to the original.
    
    Disable it for the time being.
    
    BREAKING CHANGE:
    The resulting file changes, so it is not compatible with previous version.

--- src/compress.ts
+++ src/compress.ts
@@ -1,12 +1,13 @@
-import lz from 'lz-string';
+// import lz from 'lz-string';
 import { MinifiedTestResult } from './minify';
 
 export function compress(obj: MinifiedTestResult) {
-  const str = JSON.stringify(obj)
-  return lz.compress(str)
+  return JSON.stringify(obj)
+  // return lz.compress(JSON.stringify(obj))
 }
 
-export function decompress(str: string) {
-  const json = lz.decompress(str)
-  return JSON.parse(json) as MinifiedTestResult
+export function decompress(str: string): MinifiedTestResult {
+  return JSON.parse(str)
+  // const json = lz.decompress(str)
+  // return JSON.parse(json) as MinifiedTestResult
 }
