9f60f4397ce15fda4d22982a94f68fa5f18c2482
Homa Wong <homawong@gmail.com>
2022-01-03 21:56:35 -0800

    fix: add store key

--- ts/store.ts
+++ ts/store.ts
@@ -3,6 +3,7 @@ import { createStore } from 'global-store'
 export const store = createStore<{ rootDir: string }>({
   moduleName: 'test-progress-tracker',
   version: '1.0.0',
+  key: 'eb969cee-8646-5b9c-9084-9093804d4121',
   initializer: (current) => ({
     rootDir: '.',
     ...current
